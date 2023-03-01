8b309a7ab65ebd644fe4872d0fdbe46810d71172
Giovanni Almeida <69661645+GiovanniBot@users.noreply.github.com>
2023-03-01 18:24:30 -0300

    dynamic title label

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -9,20 +9,30 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
+    @IBOutlet weak var titleLabel: UILabel!
     let eggTimes : [String: Int] = ["Soft": 5, "Medium": 7, "Hard": 12]
+    var secondsRemaining = 60
+    var timer = Timer()
     
     @IBAction func hardnessSelected(_ sender: UIButton) {
+        timer.invalidate()
+        
         let hardness = sender.currentTitle!
-        let minutes = eggTimes[hardness]
-        var seconds = minutes! * 60
         
-        repeat {
-            print(seconds)
-            Thread.sleep(forTimeInterval: 1.0)
-            seconds -= 1
-        } while (seconds > 0);
+        secondsRemaining = eggTimes[hardness]! * 60
         
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
+        
+    }
+    
+    @objc func updateTimer() {
+        if secondsRemaining > 0 {
+            titleLabel.text = "\(secondsRemaining) seconds"
+            secondsRemaining -= 1
+        } else {
+            timer.invalidate()
+            titleLabel.text = "Done!"
+        }
     }
 
 }
